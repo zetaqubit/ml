627d6d03b4d9e497d4fc1dd791187f49ae64b069
Zhao <x.zhao.y@gmail.com>
2017-11-28 00:14:33 -0800

    Reformat file. train_pg finished loss fn.

--- berkeleyrlcourse/hw2/plot.py
+++ berkeleyrlcourse/hw2/plot.py
@@ -48,72 +48,75 @@ the --legend flag and then provide a title for each logdir.
 
 """
 
+
 def plot_data(data, value="AverageReturn"):
-    if isinstance(data, list):
-        data = pd.concat(data, ignore_index=True)
-    sns.set(style="darkgrid", font_scale=1.5)
-    sns.tsplot(data=data, time="Iteration", value=value, unit="Unit", condition="Condition")
-    plt.legend(loc='best').draggable()
-    plt.show()
+  if isinstance(data, list):
+    data = pd.concat(data, ignore_index=True)
+  sns.set(style="darkgrid", font_scale=1.5)
+  sns.tsplot(data=data, time="Iteration", value=value, unit="Unit",
+             condition="Condition")
+  plt.legend(loc='best').draggable()
+  plt.show()
 
 
 def get_datasets(fpath, condition=None):
-    unit = 0
-    datasets = []
-    for root, dir, files in os.walk(fpath):
-        if 'log.txt' in files:
-            param_path = open(os.path.join(root,'params.json'))
-            params = json.load(param_path)
-            exp_name = params['exp_name']
-            
-            log_path = os.path.join(root,'log.txt')
-            experiment_data = pd.read_table(log_path)
+  unit = 0
+  datasets = []
+  for root, dir, files in os.walk(fpath):
+    if 'log.txt' in files:
+      param_path = open(os.path.join(root, 'params.json'))
+      params = json.load(param_path)
+      exp_name = params['exp_name']
 
-            experiment_data.insert(
-                len(experiment_data.columns),
-                'Unit',
-                unit
-                )
-            experiment_data.insert(
-                len(experiment_data.columns),
-                'Condition',
-                condition or exp_name
-                )
+      log_path = os.path.join(root, 'log.txt')
+      experiment_data = pd.read_table(log_path)
 
-            datasets.append(experiment_data)
-            unit += 1
+      experiment_data.insert(
+        len(experiment_data.columns),
+        'Unit',
+        unit
+      )
+      experiment_data.insert(
+        len(experiment_data.columns),
+        'Condition',
+        condition or exp_name
+      )
 
-    return datasets
+      datasets.append(experiment_data)
+      unit += 1
+
+  return datasets
 
 
 def main():
-    import argparse
-    parser = argparse.ArgumentParser()
-    parser.add_argument('logdir', nargs='*')
-    parser.add_argument('--legend', nargs='*')
-    parser.add_argument('--value', default='AverageReturn', nargs='*')
-    args = parser.parse_args()
+  import argparse
+  parser = argparse.ArgumentParser()
+  parser.add_argument('logdir', nargs='*')
+  parser.add_argument('--legend', nargs='*')
+  parser.add_argument('--value', default='AverageReturn', nargs='*')
+  args = parser.parse_args()
 
-    use_legend = False
-    if args.legend is not None:
-        assert len(args.legend) == len(args.logdir), \
-            "Must give a legend title for each set of experiments."
-        use_legend = True
+  use_legend = False
+  if args.legend is not None:
+    assert len(args.legend) == len(args.logdir), \
+      "Must give a legend title for each set of experiments."
+    use_legend = True
 
-    data = []
-    if use_legend:
-        for logdir, legend_title in zip(args.logdir, args.legend):
-            data += get_datasets(logdir, legend_title)
-    else:
-        for logdir in args.logdir:
-            data += get_datasets(logdir)
+  data = []
+  if use_legend:
+    for logdir, legend_title in zip(args.logdir, args.legend):
+      data += get_datasets(logdir, legend_title)
+  else:
+    for logdir in args.logdir:
+      data += get_datasets(logdir)
 
-    if isinstance(args.value, list):
-        values = args.value
-    else:
-        values = [args.value]
-    for value in values:
-        plot_data(data, value=value)
+  if isinstance(args.value, list):
+    values = args.value
+  else:
+    values = [args.value]
+  for value in values:
+    plot_data(data, value=value)
+
 
 if __name__ == "__main__":
-    main()
+  main()
