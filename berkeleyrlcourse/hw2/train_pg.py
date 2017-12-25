087814a479eb34118152680807e746b913537119
Zhao <x.zhao.y@gmail.com>
2017-12-24 23:26:26 -0800

    Automatically derive logdir from the overridden exp vars.

--- berkeleyrlcourse/hw2/train_pg.py
+++ berkeleyrlcourse/hw2/train_pg.py
@@ -444,29 +444,54 @@ def train_PG(exp_name='',
 def main():
   import argparse
   parser = argparse.ArgumentParser()
-  parser.add_argument('env_name', type=str)
-  parser.add_argument('--exp_name', type=str, default='vpg')
-  parser.add_argument('--render_eps', type=int, default=-1)
-  parser.add_argument('--discount', type=float, default=1.0)
-  parser.add_argument('--n_iter', '-n', type=int, default=100)
-  parser.add_argument('--batch_size', '-b', type=int, default=1000)
-  parser.add_argument('--ep_len', '-ep', type=float, default=-1.)
-  parser.add_argument('--learning_rate', '-lr', type=float, default=5e-3)
-  parser.add_argument('--reward_to_go', '-rtg', action='store_true')
-  parser.add_argument('--dont_normalize_advantages', '-dna',
-                      action='store_true')
-  parser.add_argument('--nn_baseline', '-bl', action='store_true')
-  parser.add_argument('--seed', type=int, default=1)
-  parser.add_argument('--n_experiments', '-e', type=int, default=1)
-  parser.add_argument('--n_layers', '-l', type=int, default=1)
-  parser.add_argument('--size', '-s', type=int, default=32)
+  store_actions = [
+      parser.add_argument('env_name', type=str),
+      parser.add_argument('--exp_name', type=str, default='vpg'),
+      parser.add_argument('--render_eps', type=int, default=-1),
+      parser.add_argument('--discount', type=float, default=1.0),
+      parser.add_argument('--n_iter', '-n', type=int, default=100),
+      parser.add_argument('--batch_size', '-b', type=int, default=1000),
+      parser.add_argument('--ep_len', '-ep', type=float, default=-1.),
+      parser.add_argument('--learning_rate', '-lr', type=float, default=5e-3),
+      parser.add_argument('--reward_to_go', '-rtg', action='store_true'),
+      parser.add_argument('--dont_normalize_advantages', '-dna',
+                          action='store_true'),
+      parser.add_argument('--nn_baseline', '-bl', action='store_true'),
+      parser.add_argument('--seed', type=int, default=1),
+      parser.add_argument('--n_experiments', '-e', type=int, default=1),
+      parser.add_argument('--n_layers', '-l', type=int, default=1),
+      parser.add_argument('--size', '-s', type=int, default=32),
+  ]
   args = parser.parse_args()
+
+  def get_params_str():
+    defaults = set(vars(parser.parse_args(['foo'])).items())
+    args_set = set(vars(args).items())
+    overrides = dict(args_set - defaults)
+    option_strings = [a.option_strings for a in store_actions]
+    long_to_short_names = {
+      l[0][2:]: l[1][1:] for l in option_strings if len(l) > 1
+    }
+    params_str = ''
+    for long_name in sorted(overrides.keys()):
+      if long_name in ['env_name', 'exp_name']:
+        continue
+      name = long_to_short_names.get(long_name, long_name)
+      value = overrides[long_name]
+      value_str = str(value) if type(value) != bool else ''
+      params_str += '_' if len(params_str) > 0 else ''
+      params_str += name + value_str
+    return params_str
+
+
+  #logdir = args.exp_name + '_' + args.env_name + '_' + time.strftime(
+  #  "%d-%m-%Y_%H-%M-%S")
+  logdir = '{}:{}:{}:{}'.format(args.env_name, args.exp_name, get_params_str(),
+                                time.strftime('%Y-%m-%d:%H-%M-%S'))
+  logdir = os.path.join('data', logdir)
 
   if not (os.path.exists('data')):
     os.makedirs('data')
-  logdir = args.exp_name + '_' + args.env_name + '_' + time.strftime(
-    "%d-%m-%Y_%H-%M-%S")
-  logdir = os.path.join('data', logdir)
   if not (os.path.exists(logdir)):
     os.makedirs(logdir)
 
