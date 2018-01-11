0e31c8d663a75233fabbd98c0ba918815b220e23
Zhao <x.zhao.y@gmail.com>
2018-01-10 22:12:06 -0800

    Plot 95% in addition to mean.

--- algs/plotter.py
+++ algs/plotter.py
@@ -1,21 +1,39 @@
+import collections
 from matplotlib import pyplot as plt
+import numpy as np
+
+Point = collections.namedtuple('Point', 'x y')
 
 class Plotter(object):
+
   def __init__(self):
-    self.xs = []
-    self.ys = []
+    self.points = collections.defaultdict(list)
+    self.figures = {}
 
-  def add_data(self, x, y):
-    self.xs.append(x)
-    self.ys.append(y)
+  def add_data(self, key, x, y):
+    self.points[key].append(Point(x, y))
 
-  def create_fig(self):
-    return plt.figure()
+  def create_figs(self, keys):
+    self.figures.update({k: plt.figure() for k in keys})
 
-  def line_plot(self, figure=None):
-    fig = figure or plt.figure()
-    plt.plot(self.xs, self.ys)
-    return fig
+  def get_fig(self, key):
+    return self.figures[key]
+
+  def line_plot(self):
+    for name, point_list in self.points.items():
+      plt.figure(self.figures[name].number)
+      xs = [p.x for p in point_list]
+      try:
+        iter(point_list[0].y)  # Checks if the provided ys are iterable.
+        ys = [np.mean(p.y) for p in point_list]
+        ys_low = [np.percentile(p.y, 2.5) for p in point_list]
+        ys_high = [np.percentile(p.y, 97.5) for p in point_list]
+        plt.fill_between(xs, ys_low, ys_high, alpha=0.4, label=name + ' 95%')
+      except TypeError:
+        ys = [p.y for p in point_list]
+      plt.plot(xs, ys, label=name + ' mean')
+      plt.title(name)
+      plt.legend()
 
   def render(self):
     plt.pause(0.01)
