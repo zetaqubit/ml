2e6dec42c487f0857398d11d0a7067e8ef6fc157
Zhao <x.zhao.y@gmail.com>
2018-03-04 00:42:59 -0800

    Atari: add environment that preprocesses/batches 4 frames.

--- algs/environment.py
+++ algs/environment.py
@@ -4,9 +4,12 @@ import collections
 
 import gym
 import numpy as np
+from skimage import color
+from skimage import transform
 
 
 SAR = collections.namedtuple('SAR', 's a r')
+SARS = collections.namedtuple('SARS', 's a r s1')
 
 
 class Environment(object):
@@ -76,3 +79,56 @@ class Environment(object):
       self.reset()
 
 
+class AtariEnvironment:
+  def __init__(self, env_name, action_repeat=4):
+    self.env_name = env_name + 'NoFrameskip-v4'
+    self.action_repeat = action_repeat
+    self.env = gym.make(self.env_name)
+
+    # Observation and action sizes
+    self.discrete_ac = isinstance(self.env.action_space, gym.spaces.Discrete)
+    self.raw_obs_shape = self.env.observation_space.shape
+    self.obs_dim = (action_repeat, 84, 84)
+    self.acs_dim = (self.env.action_space.n if self.discrete_ac
+                    else self.env.action_space.shape[0])
+
+    self.last_obs = self.reset()
+
+  def reset(self):
+    obs = np.empty(self.obs_dim)
+    obs[0, :, :] = self._preprocess(self.env.reset())
+    # TODO: verify assumption that action 0 is No-op.
+    obs[1:, :, :], r, done = self.step_k(0, self.action_repeat - 1)
+    assert not done  # TODO: handle
+    return obs
+
+  def _preprocess(self, ob):
+    """Converts 210x160x3 RGB to 84x84x1 Y."""
+    y = color.rgb2yuv(ob)[:, :, 0]
+    y_resized = transform.resize(y, (84, 84))
+    return y_resized
+
+  def step_k(self, ac, k):
+    obs = np.zeros((k,) + self.obs_dim[1:])
+    r_sum = 0
+    done = False
+    for i in range(k):
+      rgb, r, done, _ = self.env.step(ac)
+      obs[i, :, :] = self._preprocess(rgb)
+      r_sum += r  # TODO: might need to discount
+      if done:
+        break
+    return obs, r_sum, done
+
+  def step(self, policy):
+    assert self.last_obs is not None
+    print(self.last_obs.shape)
+    ac = policy(np.expand_dims(self.last_obs, 0)).squeeze().astype(int)
+    obs, r, done = self.step_k(ac, self.action_repeat)
+    if not done:
+      sars = SARS(self.last_obs, ac, r, obs)
+      self.last_obs = obs
+    else:
+      sars = SARS(self.last_obs, ac, r, None)
+      self.last_obs = self.reset()
+    return sars
