aea63add539c576dbcb21bc691ee111e44b40c48
Zhao <x.zhao.y@gmail.com>
2018-08-05 10:33:26 -0700

    box2d: simple env with boundary and dynamic boxes.

--- box2d/evo/environment.py
+++ box2d/evo/environment.py
@@ -4,8 +4,11 @@ Provides per-frame simulation, event callbacks, and input handling.
 """
 from rl.box2d.app import framework
 
+from Box2D import b2
+
 
 class Environment(framework.Framework):
+
   name = "2D Environment"  # Name of the class to display
   description = "2D physics simulation"
 
@@ -14,9 +17,35 @@ class Environment(framework.Framework):
     Initialize all of your objects here.
     Be sure to call the Framework's initializer first.
     """
-    super().__init__()
+    super().__init__(gravity=(0, 0))
 
-    # Initialize all of the objects
+    # wall_body = self.world.CreateBody(position=(0, -10))
+    # wall_box = b2.polygonShape(box=(50, 10))
+    # wall_body.CreateFixture(shape=wall_box)
+
+    # wall_body = self.world.CreateStaticBody(
+    #     position=(0, -10),
+    #     shapes=[b2.polygonShape(box=(50, 10)),
+    #             b2.polygonShape(box=(10, 50))],
+    # )
+
+    # The boundaries.
+    play_area = self.world.CreateBody(position=(0, 20))
+    play_area.CreateEdgeChain(
+      [(-20, -20), (-20, 20), (20, 20), (20, -20), (-20, -20)]
+    )
+
+    box1 = self.world.CreateDynamicBody(position=(0, 4))
+    box1.CreatePolygonFixture(box=(1, 1), density=1, friction=0.3)
+
+    box2 = self.world.CreateDynamicBody(
+      position=(3, 4),
+      angle=0,
+      linearDamping=1,
+      angularDamping=1,
+      shapes=b2.polygonShape(box=(1, 1)),
+      shapeFixture=b2.fixtureDef(density=1, friction=0.3)
+    )
 
   def Keyboard(self, key):
     """
