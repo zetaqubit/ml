b539077f6b7d665abad6e46c1dda6f8abb8046ed
Zhao <x.zhao.y@gmail.com>
2019-06-02 11:24:18 -0700

    Add ClutteredMnistSyntheticDataset.

--- rl/core/envs/synthetic_dataset.py
+++ rl/core/envs/synthetic_dataset.py
@@ -125,15 +125,45 @@ class MnistSyntheticDataset(SyntheticDataset):
     """
     mnist_dataset = tv.datasets.MNIST(
         mnist_dir, train, transform=tv.transforms.ToTensor(), download=True)
-    images = mnist_dataset.data.numpy()
-    labels = mnist_dataset.targets.numpy()
+    self.orig_images = mnist_dataset.data.numpy()
+    self.orig_labels = mnist_dataset.targets.numpy()
 
     # [n, h, w] -> [n, c, h, w]
-    images = np.expand_dims(images, axis=1)
+    self.orig_images = np.expand_dims(self.orig_images, axis=1)
 
     super().__init__(width, height, num_images, tiles_per_image,
-                     tile_images=images, tile_labels=labels, label_fn=label_fn,
-                     seed=seed)
+                     tile_images=self.orig_images,
+                     tile_labels=self.orig_labels,
+                     label_fn=label_fn, seed=seed)
+
+
+class ClutteredMnistSyntheticDataset(SyntheticDataset):
+  def __init__(self, width, height, num_images, true_tiles_per_image,
+               clutter_tiles_per_image, mnist_dir='~/code/data/mnist',
+               train=True, label_fn=np.sum, seed=0, merge_mode='max'):
+
+    # Create uncluttered dataset.
+    true_ds = MnistSyntheticDataset(
+        width, height, num_images, true_tiles_per_image,
+        mnist_dir=mnist_dir, train=train, label_fn=label_fn, seed=seed)
+
+    # Create clutter tiles by splitting each true tile into 2x2.
+    clutter_tiles = np.concatenate(np.split(true_ds.orig_images, 2, axis=2))
+    clutter_tiles = np.concatenate(np.split(clutter_tiles, 2, axis=3))
+    clutter_labels = np.tile(true_ds.orig_labels, 4)
+    clutter_ds = SyntheticDataset(
+        width, height, num_images, clutter_tiles_per_image, clutter_tiles,
+        clutter_labels)
+
+    # Merge with clutter background.
+    # TODO: fix fifo - it replaces entire image rather than just foreground.
+    merge_fn = _get_merge_fn(merge_mode)
+    self._images = merge_fn(true_ds.images, clutter_ds.images)
+    self._labels = true_ds.labels
+
+    th.utils.data.TensorDataset.__init__(self,
+                                         th.Tensor(self._images),
+                                         th.Tensor(self._labels))
 
 
 def tile_image(tiles, tile_xys, width, height, merge_mode='max'):
@@ -152,13 +182,7 @@ def tile_image(tiles, tile_xys, width, height, merge_mode='max'):
       - 'fifo': tiles are painted in order. Take the pixel of the last tile.
   :return: the merged image, as an np.array shaped [c, height, width]
   """
-  merge_fns = {
-    'max': np.maximum,
-    'sum': np.add,
-    'fifo': lambda old, new: new,
-  }
-  assert merge_mode in merge_fns.keys()
-  merge_fn = merge_fns[merge_mode]
+  merge_fn = _get_merge_fn(merge_mode)
 
   channels = tiles[0].shape[0]
   image = np.zeros((channels, height, width))
@@ -167,3 +191,13 @@ def tile_image(tiles, tile_xys, width, height, merge_mode='max'):
     old = image[:, y:y + tile_h, x:x + tile_w]
     image[:, y:y + tile_h, x:x + tile_w] = merge_fn(old, tile)
   return image
+
+
+def _get_merge_fn(merge_mode):
+  merge_fns = {
+      'max': np.maximum,
+      'sum': np.add,
+      'fifo': lambda old, new: new,
+  }
+  assert merge_mode in merge_fns.keys()
+  return merge_fns[merge_mode]
