bd0e38c06b57f745e86758041e13065c1386f0dd
Zhao <x.zhao.y@gmail.com>
2019-06-14 04:29:37 -0700

    Start switching from unittest to pytest.

--- rl/box2d/newton/box2d_log_test.py
+++ rl/box2d/newton/box2d_log_test.py
@@ -1,47 +1,42 @@
 """Tests for rl.box2d.newton.box2d_log."""
 
 import io
-import unittest
 
 import pandas as pd
 
 from rl.box2d.newton import box2d_log
 
 
-class CsvLogTest(unittest.TestCase):
-  def setup(self):
-    pass
+def test_add(tmp_path):
+  log = box2d_log.CsvLog(log_filepath=tmp_path)
+  row = {'t': 1, 'a': 2, 'b': 3}
+  log.add(row)
+  expected = pd.DataFrame([row], columns=['t', 'a', 'b'])
+  pd.testing.assert_frame_equal(expected, log.as_df())
 
-  def test_add(self):
-    log = box2d_log.CsvLog(None)
-    row = {'t': 1, 'a': 2, 'b': 3}
-    log.add(row)
-    expected = pd.DataFrame([row])
-    pd.testing.assert_frame_equal(expected, log.as_df())
+def test_add_different_keys(tmpdir):
+  log = box2d_log.CsvLog(log_dir=tmpdir)
+  rows = [
+      {'t': 1, 'a': 2, 'b': 3},
+      {'t': 2, 'c': 4, 'b': 5},
+  ]
+  log.add(rows[0])
+  log.add(rows[1])
+  expected = pd.DataFrame(rows, columns=['t', 'a', 'b', 'c'])
+  pd.testing.assert_frame_equal(expected, log.as_df())
 
-  def test_add_different_keys(self):
-    log = box2d_log.CsvLog(None)
-    rows = [
-        {'t': 1, 'a': 2, 'b': 3},
-        {'t': 2, 'c': 4, 'b': 5},
-    ]
-    log.add(rows[0])
-    log.add(rows[1])
-    expected = pd.DataFrame(rows)
-    pd.testing.assert_frame_equal(expected, log.as_df())
+def test_write():
+  fp = io.StringIO()
+  log = box2d_log.CsvLog(fp)
+  rows = [
+      {'t': 1, 'a': 2, 'b': 3},
+      {'t': 2, 'c': 4, 'b': 5},
+  ]
+  log.add(rows[0])
+  log.add(rows[1])
+  log.write()
 
-  def test_write(self):
-    fp = io.StringIO()
-    log = box2d_log.CsvLog(fp)
-    rows = [
-        {'t': 1, 'a': 2, 'b': 3},
-        {'t': 2, 'c': 4, 'b': 5},
-    ]
-    log.add(rows[0])
-    log.add(rows[1])
-    log.write()
-
-    fp.seek(0)
-    actual = pd.read_csv(fp)
-    pd.testing.assert_frame_equal(log.as_df(), actual)
+  fp.seek(0)
+  actual = pd.read_csv(fp)
+  pd.testing.assert_frame_equal(log.as_df(), actual)
 
--- rl/box2d/newton/box2d_state_test.py
+++ rl/box2d/newton/box2d_state_test.py
@@ -1,56 +1,58 @@
 """Tests for rl.box2d.newton.box2d_state."""
 
-import unittest
-
 from Box2D import b2
+import pytest
 
 from rl.box2d.newton import box2d_state
 
 
-class Box2dStateTest(unittest.TestCase):
-  def setUp(self):
-    self.state1 = box2d_state.ObjectState(position=(1, 2))
-    self.state2 = box2d_state.ObjectState(position=(3, 4))
-    self.world_state = box2d_state.WorldState(objects={
-        'a': self.state1,
-        'b': self.state2,
-    }, gravity=(0, -9.81))
+@pytest.fixture
+def state1():
+  return box2d_state.ObjectState(position=(1, 2))
 
-  def test_object_state_construct(self):
-    world = b2.world()
-    obj = self.state1.construct(world)
-    self.assertEqual(self.state1.position, obj.position)
-    self.assertEqual(self.state1.velocity, obj.linearVelocity)
-    self.assertEqual(self.state1.angle, obj.angle)
-    self.assertEqual(self.state1.angular_velocity, obj.angularVelocity)
-    self.assertEqual(self.state1.linear_damping, obj.linearDamping)
-    self.assertEqual(self.state1.angular_damping, obj.angularDamping)
-    self.assertEqual(self.state1.fixed_rotation, obj.fixedRotation)
+@pytest.fixture
+def state2():
+  return box2d_state.ObjectState(position=(1, 2))
 
-    self.assertEqual(self.state1.friction, obj.fixtures[0].friction)
-    self.assertEqual(self.state1.density, obj.fixtures[0].density)
-    self.assertEqual(self.state1.restitution, obj.fixtures[0].restitution)
+@pytest.fixture
+def world_state(state1, state2):
+  return box2d_state.WorldState(objects={
+      'a': state1,
+      'b': state2,
+  }, gravity=(0, -9.81))
 
-  def test_world_state_construct(self):
-    world = self.world_state.construct()
-    self.assertEqual(self.world_state.gravity, world.gravity)
-    self.assertEqual(2, world.bodyCount)
-    expected_world = b2.world()
-    expected_a = self.state1.construct(expected_world)
-    self.assertEqual(expected_a.position, world.objects['a'].position)
-    expected_b = self.state2.construct(expected_world)
-    self.assertEqual(expected_b.position, world.objects['b'].position)
+def test_object_state_construct(state1):
+  world = b2.world()
+  obj = state1.construct(world)
+  assert obj.position == state1.position
+  assert obj.linearVelocity == state1.velocity
+  assert obj.angle == state1.angle
+  assert obj.angularVelocity == state1.angular_velocity
+  assert obj.linearDamping == state1.linear_damping
+  assert obj.angularDamping == state1.angular_damping
+  assert obj.fixedRotation == state1.fixed_rotation
 
-  def test_object_state_json(self):
-    json = self.state1.to_json()
-    deserialized = box2d_state.ObjectState.from_json(json)
-    self.assertEqual(self.state1, deserialized)
+  assert obj.fixtures[0].friction == state1.friction
+  assert obj.fixtures[0].density == state1.density
+  assert obj.fixtures[0].restitution == state1.restitution
 
-  def test_world_state_json(self):
-    json = self.world_state.to_json()
-    deserialized = box2d_state.WorldState.from_json(json)
-    self.assertEqual(self.world_state, deserialized)
+def test_world_state_construct(world_state, state1, state2):
+  world = world_state.construct()
+  assert world.gravity == world_state.gravity
+  assert world.bodyCount == 2
+  expected_world = b2.world()
+  expected_a = state1.construct(expected_world)
+  assert world.objects['a'].position == expected_a.position
+  expected_b = state2.construct(expected_world)
+  assert world.objects['b'].position == expected_b.position
 
+def test_object_state_json(state1):
+  json = state1.to_json()
+  deserialized = box2d_state.ObjectState.from_json(json)
+  assert deserialized == state1
 
-if __name__ == '__main__':
-  unittest.main()
+def test_world_state_json(world_state):
+  json = world_state.to_json()
+  deserialized = box2d_state.WorldState.from_json(json)
+  assert deserialized == world_state
+
